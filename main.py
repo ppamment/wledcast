07ad18d786ddcf7735480e63a3e84abab8f0820f
Paul Pamment <p.pamment@gmail.com>
2023-11-30 00:17:28 +0000

    Refactor message check in main loop to simplify code.

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     try:
         while True:
             msg = win32gui.GetMessage(None, 0, 0)
-            if msg[1].message == 0 or msg[1].message == -1:
+            if msg[1] == 0 or msg[1] == -1:
                 break
             win32gui.TranslateMessage(msg)
             win32gui.DispatchMessage(msg)
